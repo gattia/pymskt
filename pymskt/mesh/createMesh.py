6a1a29fdfefd0987f7c9307b6e5f982daf6449ad
Anthony Gatt <anthony@neuralseg.com>
2022-01-18 21:06:08 -0800

    Add docstring for

--- pymskt/mesh/createMesh.py
+++ pymskt/mesh/createMesh.py
@@ -15,19 +15,35 @@ def discrete_marching_cubes(vtk_image_reader,
                             ):
     """
     Compute dmc on segmentation image.
+    Creates a surface mesh (polydata) that closely covers binary (discrete) segmentations.
 
     Parameters
     ----------
-    vtk_image_reader
-    compute_normals_on
-    n_contours
-    contours_range_start
-    contours_range_end
+    vtk_image_reader : vtk.Filter
+        VTK pipeline to apply discrete marching cubes to. 
+    n_labels : int, optional
+        Number of labes to create mesh for, by default 1
+    start_label : int, optional
+        Starting index of labels to mesh, by default 1
+    end_label : int, optional
+        Ending index of labels to mesh, by default 1
+    compute_normals_on : bool, optional
+        Calculate normals to surface, by default True
+    return_polydata : bool, optional
+        Whether to return a vtk.polydata or not (VTK filter pipeline instead e.g., `dmc`), by default True
 
     Returns
     -------
+    vtk.Filter Pipeline
+        Returns a pipeline which more functions can be chained too - this improves performance.
+    
+    OR
 
-    """
+    vtk.Polydata
+        Returns a polydata (surface mesh). 
+
+    """    
+
     dmc = vtk.vtkDiscreteMarchingCubes()
     dmc.SetInputConnection(vtk_image_reader.GetOutputPort())
     if compute_normals_on is True:
@@ -46,6 +62,35 @@ def continuous_marching_cubes(vtk_image_reader,
                               compute_normals_on=True,
                               compute_gradients_on=True,
                               return_polydata=True):
+    """
+    - Compute a continuous marching cubes on a segmentation mask. 
+    - Enables defining the surface based on a contour set to a floating point cutoff. 
+
+
+    Parameters
+    ----------
+    vtk_image_reader : vtk.Filter
+        This is the output of a previous vtk filter from a previous step. E.g., output of pymskt.image.read_nrrd().
+        
+    threshold : float, optional
+        Floating point value to create surface mesh, by default 0.5
+    compute_normals_on : bool, optional
+        Whether or not to compute surface normals for mesh, by default True
+    compute_gradients_on : bool, optional
+        Whether or not to compute gradients over mesh surface, by default True
+    return_polydata : bool, optional
+        Whether to return a vtk.polydata or not (VTK filter pipeline instead e.g., `mc`), by default True
+
+    Returns
+    -------
+    vtk.Filter Pipeline
+        Returns a pipeline which more functions can be chained too - this improves performance.
+    
+    OR
+
+    vtk.Polydata
+        Returns a polydata (surface mesh). 
+    """    
     mc = vtk.vtkMarchingContourFilter()
     mc.SetInputConnection(vtk_image_reader.GetOutputPort())
     if compute_normals_on is True:
@@ -75,7 +120,31 @@ def create_surface_mesh_smoothed(seg_image,
                                  mc_threshold=0.5):
     """
     Create surface mesh based on a filtered binary image to try and get smoother surface representation
-    """
+
+    Parameters
+    ----------
+    seg_image : SimpleITK.Image
+        Segmentation image to be filtered and meshed with marching cubes. 
+    label_idx : int
+        What anatomical label to be meshed.
+    image_smooth_var : float
+        Variance to apply a gaussian smoothing function to. 
+    loc_tmp_save : str, optional
+        Location to save temporary files for passing SimpleITK.Image to vtk functions, by default '/tmp'
+    tmp_filename : str, optional
+        Filename of saved temporary file, by default 'temp_smoothed_bone.nrrd'
+    copy_image_transform : bool, optional
+        Whether or not to apply image transform to final mesh or to leave it at origin, by default True
+    mc_threshold : float, optional
+        What floating point value to create surface mesh at, by default 0.5
+
+    Returns
+    -------
+    vtk.Polydata
+        Surface mesh created using a continuous cutoff `mc_threshold` after applying 
+        gaussian smoothing with variance = `image_smooth_var`.
+    """    
+
     # Set border of segmentation to 0 so that segs are all closed.
     seg_image = msktimage.set_seg_border_to_zeros(seg_image, border_size=1)
 
