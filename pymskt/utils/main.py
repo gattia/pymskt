6350b22a94f1aa6bb6245a61d2ac762b9061dd91
Anthony Gatt <anthony@neuralseg.com>
2022-01-20 10:08:12 -0800

    adding helper function useful for testing

--- pymskt/utils/main.py
+++ pymskt/utils/main.py
@@ -1,7 +1,11 @@
 import vtk
+import SimpleITK as sitk
 import numpy as np
 import os
 import errno
+
+from numpy.testing import assert_allclose
+
 
 def l2n(l):
     """
@@ -151,3 +155,22 @@ def safely_delete_tmp_file(location,
         if exc.errno != errno.ENOENT:
             raise
         pass
+
+def assert_images_same(image1, image2):
+    """
+    Helper function to assert that 2 SimpleITK images are the same. 
+
+    Parameters
+    ----------
+    image1 : SimpleITK.Image
+        Version 1 of the image. 
+    image2 : SimpleITK.Image
+        Version 2 of the image. 
+    """    
+    image1_array = sitk.GetArrayFromImage(image1)
+    image2_array = sitk.GetArrayFromImage(image2)
+
+    assert_allclose(image1_array, image2_array)
+    assert image1.GetOrigin() == image2.GetOrigin()
+    assert image1.GetSpacing() == image2.GetSpacing()
+    assert image1.GetDirection() == image2.GetDirection()
