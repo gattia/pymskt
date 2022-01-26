3ea4f191d5e651e4fd4f1985dd2bdf1daa954670
Anthony Gatt <anthony@neuralseg.com>
2022-01-25 17:45:26 -0800

    reformatting testing structure and adding new files

--- testing/bone_surface_data_test.py
+++ testing/bone_surface_data_test.py
@@ -1,18 +1,19 @@
-from operator import sub
 import pytest
 import pymskt as mskt
 import numpy as np
 import SimpleITK as sitk
 from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
 from numpy.testing import assert_allclose
+from pymskt.utils import testing
 
 
 try:
-    downsampled_femur_mesh = mskt.mesh.io.read_vtk('data/femur_thickness_mm_10k_pts.vtk')
+    # downsampled_femur_mesh = mskt.mesh.io.read_vtk('data/femur_thickness_mm_10k_pts.vtk')
+    downsampled_femur_mesh = mskt.mesh.io.read_vtk('data/femur_thickness_mm_regions_10k_pts.vtk')
     cropped_femur_mesh = mskt.mesh.io.read_vtk('data/femur_cropped_cartilage_thick_roi_10k_pts.vtk')
     smoothed_femur_mesh = mskt.mesh.io.read_vtk('data/femur_cropped_cartilage_thick_smoothed_1.25_sigma_10k_pts.vtk')
 except OSError:
-    downsampled_femur_mesh = mskt.mesh.io.read_vtk('../data/femur_thickness_mm_10k_pts.vtk')
+    downsampled_femur_mesh = mskt.mesh.io.read_vtk('../data/femur_thickness_mm_regions_10k_pts.vtk')
     cropped_femur_mesh = mskt.mesh.io.read_vtk('../data/femur_cropped_cartilage_thick_roi_10k_pts.vtk')
     smoothed_femur_mesh = mskt.mesh.io.read_vtk('../data/femur_cropped_cartilage_thick_smoothed_1.25_sigma_10k_pts.vtk')
 
@@ -56,12 +57,12 @@ def test_cropped_femur_cartilage_region_assignment():
         
     """     
     femur = mskt.mesh.BoneMesh(path_seg_image='data/right_knee_example.nrrd', 
-                               label_idx=5, 
-                               list_cartilage_labels=[1], 
-                               crop_percent=0.7)
+                                label_idx=5, 
+                                list_cartilage_labels=[1], 
+                                crop_percent=0.7)
     femur.create_mesh()
     femur.resample_surface()
-    # femur.calc_cartilage_thickness()  # DONT NEED THIS FOR THIS TEST.... MAYBE CREATE NEW TEST INCLUDING THIS? 
+    femur.calc_cartilage_thickness()
     femur.assign_cartilage_regions()
 
     # compare cartilage ROI between new calculation & saved mesh. 
@@ -106,40 +107,56 @@ def test_femur_cart_thick_roi_calc(timing=False):
     explicitly, instead of implicitly by passing cartilage labels to `BoneMesh` such as
     `list_cartilage_labels`. 
     """    
-    femur = mskt.mesh.BoneMesh(path_seg_image='data/right_knee_example.nrrd', label_idx=5)
+    femur = mskt.mesh.BoneMesh(
+        path_seg_image='data/right_knee_example.nrrd', 
+        label_idx=5,
+        list_cartilage_labels=[1]
+    )
     femur.create_mesh()
     femur.resample_surface(subdivisions=2, clusters=10000)
+    femur.calc_cartilage_thickness()
+    femur.assign_cartilage_regions()
 
-    seg_vtk_image_reader = mskt.image.read_nrrd('data/right_knee_example.nrrd', set_origin_zero=True)
-    seg_vtk_image = seg_vtk_image_reader.GetOutput()
-    seg_sitk_image = sitk.ReadImage('data/right_knee_example.nrrd')
-    seg_transformer = mskt.mesh.meshTransform.SitkVtkTransformer(seg_sitk_image)
+    testing.assert_mesh_coordinates_same(femur.mesh, downsampled_femur_mesh)
+    testing.assert_mesh_scalars_same(femur.mesh, downsampled_femur_mesh, scalarname='thickness (mm)')
+    testing.assert_mesh_scalars_same(femur.mesh, downsampled_femur_mesh, scalarname='labels')
 
-    fem_cartilage = mskt.mesh.BoneMesh(path_seg_image='data/right_knee_example.nrrd', label_idx=1)
-    fem_cartilage.create_mesh()
+    # seg_vtk_image_reader = mskt.image.read_nrrd('data/right_knee_example.nrrd', set_origin_zero=True)
+    # seg_vtk_image = seg_vtk_image_reader.GetOutput()
+    # seg_sitk_image = sitk.ReadImage('data/right_knee_example.nrrd')
+    # seg_transformer = mskt.mesh.meshTransform.SitkVtkTransformer(seg_sitk_image)
 
-    fem_cartilage.apply_transform_to_mesh(transform=seg_transformer.get_inverse_transform())
-    femur.apply_transform_to_mesh(transform=seg_transformer.get_inverse_transform())
+    # fem_cartilage = mskt.mesh.BoneMesh(path_seg_image='data/right_knee_example.nrrd', label_idx=1)
+    # fem_cartilage.create_mesh()
+
+    # fem_cartilage.apply_transform_to_mesh(transform=seg_transformer.get_inverse_transform())
+    # femur.apply_transform_to_mesh(transform=seg_transformer.get_inverse_transform())
 
 
-    result = mskt.mesh.meshTools.get_cartilage_properties_at_points(femur._mesh, fem_cartilage._mesh, seg_vtk_image=seg_vtk_image, ray_cast_length=7., percent_ray_length_opposite_direction=0.25)
+    # result = mskt.mesh.meshTools.get_cartilage_properties_at_points(
+    #     femur._mesh, 
+    #     femur.list_cartilage_meshes[0]._mesh, 
+    #     seg_vtk_image=seg_vtk_image, 
+    #     ray_cast_length=10., 
+    #     percent_ray_length_opposite_direction=0.25
+    # )
 
-    thickness_scalars = numpy_to_vtk(result[0])
-    thickness_scalars.SetName('thickness (mm)')
-    femur._mesh.GetPointData().AddArray(thickness_scalars)
+    # thickness_scalars = numpy_to_vtk(result[0])
+    # thickness_scalars.SetName('thickness (mm)')
+    # femur._mesh.GetPointData().AddArray(thickness_scalars)
 
-    cartilage_roi = numpy_to_vtk(result[1])
-    cartilage_roi.SetName('cartilage_region')
-    femur._mesh.GetPointData().AddArray(cartilage_roi)
+    # cartilage_roi = numpy_to_vtk(result[1])
+    # cartilage_roi.SetName('cartilage_region')
+    # femur._mesh.GetPointData().AddArray(cartilage_roi)
 
-    femur.reverse_all_transforms()
-    fem_cartilage.reverse_all_transforms()
+    # femur.reverse_all_transforms()
+    # fem_cartilage.reverse_all_transforms()
 
-    downsampled_femur_mesh_thickness = vtk_to_numpy(downsampled_femur_mesh.GetPointData().GetArray('thickness (mm)'))
-    downsampled_femur_mesh_cart_roi = vtk_to_numpy(downsampled_femur_mesh.GetPointData().GetArray('cartilage_region'))
+    # downsampled_femur_mesh_thickness = vtk_to_numpy(downsampled_femur_mesh.GetPointData().GetArray('thickness (mm)'))
+    # downsampled_femur_mesh_cart_roi = vtk_to_numpy(downsampled_femur_mesh.GetPointData().GetArray('labels'))
 
-    assert_allclose(result[0], downsampled_femur_mesh_thickness)
-    assert_allclose(result[1], downsampled_femur_mesh_cart_roi)
+    # assert_allclose(result[0], downsampled_femur_mesh_thickness)
+    # assert_allclose(result[1], downsampled_femur_mesh_cart_roi)
 
 if __name__ == "__main__":
     import time
---
+++ testing/mesh/meshTransform/meshTransform_apply_transform_to_mesh_test.py
@@ -0,0 +1,24 @@
+import pymskt as mskt
+import vtk
+import numpy as np
+from vtk.util.numpy_support import vtk_to_numpy
+from numpy.testing import assert_allclose
+
+
+MESH = mskt.mesh.io.read_vtk('data/femur_mesh_10k_pts.vtk')
+
+def test_translation_with_transform(mesh=MESH):
+    orig_points = vtk_to_numpy(mesh.GetPoints().GetData())
+
+    translation = [10, 20, 30]
+    fourXfour = np.identity(4)
+    fourXfour[:3, 3] = translation
+
+    transform = vtk.vtkTransform()
+    transform.SetMatrix(fourXfour.flatten())
+
+    transformed_mesh = mskt.mesh.meshTransform.apply_transform(mesh, transform)    
+    transformed_points = vtk_to_numpy(transformed_mesh.GetPoints().GetData())
+
+    assert_allclose(transformed_points - translation, orig_points)
+
